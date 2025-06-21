6205e926f96074cdfd066f2110725a7a2fb48847
Leah Kvares <lk3053@g.rit.edu>
2025-06-21 00:47:57 -0400

    updated about desc

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -14,37 +14,29 @@ const About: React.FC = (): React.ReactElement => {
       </div>
       <div className="content">
         <div className="card">
-          <h2>General</h2>
-          Each year, colleges from around the country compete for the coveted title of ISTS champion. Competitors are faced with a wide
-          variety of challenges which are designed to cover as many facets of computing security, system administration, networking, and
-          programming as we possibly can. These challenges include code review, architecture design, incident response, and policy writing
-          -- all while defending a completely student-built infrastructure.
+          <h2>Overview</h2>
+          Each spring, ISTS brings together eighteen collegiate teams from across the country for a hands-on, student-built 
+          cybersecurity competition to test a wide range of skills. Selected universities send teams of five students, and RIT 
+          completes the roster with the winners of our internal red vs. blue competition, {" "}<a href="https://irsec.club" target="_blank" rel="noreferrer">IRSeC</a>{" "}. 
+          Over the course of the competition, teams face a barrage of technical challenges that test their skills in system hardening, 
+          incident response, technical writing, and more. The competition is designed to be a fun and educational experience, 
+          allowing students to apply their knowledge while also learning from their peers and industry professionals.
+          <br>
+          ISTS is designed and driven entirely by students from RITSEC, the student-run cybersecurity club at the Rochester Institute of Technology. 
+          From planning and scheduling, to infrastructure deployment and challenge development, every aspect of the competition is built from the ground up by RITSEC club members.
         </div>
         <div className="card">
-          <h2>How are we different?</h2>
-          From day one of competition planning, ISTS is built from the ground up by students. All of the planning, scheduling, building, and
-          outreach is done by members of RITSEC. Additionally, competitor skills are put to the test by adding an external red team of
-          security professionals including industry partners and passionate alumni. The red team performs offensive actions against the
-          competitors during the entire competition. Notable red team members of past ISTS competitions include{" "}
-          <a href="Raphael Mudge" target="_blank" rel="noreferrer">
-            Raphael Mudge
-          </a>{" "}
-          (creator of Cobalt Strike),{" "}
-          <a href="https://twitter.com/carnal0wnage" target="_blank" rel="noreferrer">
-            Chris Gates
-          </a>{" "}
-          (carnal0wnage), and{" "}
-          <a href="https://twitter.com/mubix/" target="_blank" rel="noreferrer">
-            Rob Fuller
-          </a>{" "}
-          (Mubix).
-        </div>
-        <div className="card">
-          <h2>Who?</h2>
-          ISTS is hosted by RITSEC, the student-run computer security club at the Rochester Institute of Technology. All of the event
-          planning, content creation, and day-of volunteering is done by dedicated members of RITSEC. All teams that compete at ISTS are
-          composed entirely of college students. Each year, several universities around the country are invited to send a team to ISTS. RIT
-          students form the remaining teams.
+          <h2>What makes us different?</h2>
+          ISTS uniquely integrates purple teaming in addition to the traditional red vs. blue format, meaning that blue teams are 
+          permitted to attack each others' systems. The competition also includes a King of the Hill (KotH) component, where teams 
+          race to take and hold control of specific vulnerable systems. Additionally, a capture-the-flag (CTF) is open throughout the 
+          weekend for an extra layer of challenges. Finally, ISTS features “The Game,” a theme-specific minigame that changes year to 
+          year. These fast-paced elements come together to let competitors demonstrate their full range of skills; there's never a dull moment.
+          <br>
+          The competition is supported by multiple student-led roles. The black team is responsible for building and managing the technical 
+          environment that powers the event. The white team supports day-of competition logistics and acts as the bridge between blue teams 
+          and the black team. Meanwhile, the red team, composed of RITSEC alumni and other industry professionals, delivers offensive pressure 
+          throughout the weekend, simulating real-world adversaries to challenge the teams' defensive skills.
         </div>
       </div>
     </div>
